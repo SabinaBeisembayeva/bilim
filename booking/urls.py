61171dcded0337767b4a6d25c8c351ce9bd27c99
beketx <beketsk@gmail.com>
2022-05-25 16:12:10 +0600

    last

--- booking/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from django.urls import path, include
-
-from booking.views import BookingView
-
-router = DefaultRouter()
-router.register(r'booking', BookingView, basename='booking')
